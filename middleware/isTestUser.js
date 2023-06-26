f7e90cf1705343f3070022cf54b5b8368e30a43b
Dan Berezin <Bogdanlinee@gmail.com>
2023-06-26 20:35:36 +0300

    Bug fix

--- middleware/isTestUser.js
+++ middleware/isTestUser.js
@@ -4,6 +4,7 @@ const isTestUser = async (req, res, next) => {
   if (req.user.userId === '6488ac5195141620c06cdaa0') {
     throw new BadRequestError('Read only account.');
   }
+  next();
 }
 
 module.exports = isTestUser;
